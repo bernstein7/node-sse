2284b7baabbb80e9666a1f0fd0b3f71966b81151
Dmitriy Kuzminov <dmitriy.kuzminov@kaeuferportal.de>
2019-03-18 09:52:33 +0100

    Connect to MQTT and stream data over SSE

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var mqtt = require('mqtt')
+var client  = mqtt.connect('mqtt://192.168.0.3:1883', {
+  username: "bernstein7",
+  password: "superSecure"
+});
+
+client.on('connect', function () {
+  console.log('Connected')
+  client.subscribe('garage/front-door', function (err) {
+    if (!err) {
+      const msg = `data: ${JSON.stringify({subscribed: true})} \n\n`
+      console.log('Init message' + msg)
+    }
+  })
+})
 
 app.get('/', function(req, res) {
-  // res.send('<h1>Hello world</h1>');
   res.sendFile(__dirname + '/index.html');
 });
 
@@ -11,19 +25,22 @@ app.get('/sse', function(req, res) {
     "connection": "keep-alive",
     "cache-control": "no-cache",
     "content-type": "text/event-stream"
-    // "content-type": "application/json"
   });
 
   let data = {
     message: "Hello world"
   }
 
-  setInterval(() => {
+  client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log(message.toString())
     data.timestamp = Date.now()
+    data.value = JSON.parse(message.toString()).value;
     const msg = `data: ${JSON.stringify(data)} \n\n`
-    console.log(msg)
+    console.log('Sending message' + msg)
     res.write(msg)
-  }, 3000)
+    // client.end()
+  })
 });
 
 http.listen(3333, function() {
